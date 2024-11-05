acd1558a3f616018e8536a61ed21fbbcaf73a145
dutchcube <leon.hanewacker@gmail.com>
2024-11-05 13:36:13 +0100

    security

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB2-rZChMrwk5ikreB0Sou7HZDuqIoyiQk',
-  authDomain: 'worko-efe21.firebaseapp.com',
-  projectId: 'worko-efe21',
-  storageBucket: 'worko-efe21.appspot.com',
-  messagingSenderId: '508557933955',
-  appId: '1:508557933955:web:a5eaffd266e6525c0c4ab6'
-}
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
